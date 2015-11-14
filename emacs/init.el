2134273f5189c676aca4a12220b35d40d4f00d68
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2015-11-14 03:48:02 +0100

    updated emacs/init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,31 +1,32 @@
+;;; UI
 
+## elements
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(setq inhibit-startup-screen t)
+
+;; color
+(add-to-list 'default-frame-alist '(foreground-color . "#C2C2C2"))
+(add-to-list 'default-frame-alist '(background-color . "#000000"))
+
+;; controls
+
+(require 'mouse)
+(xterm-mouse-mode t)
+
+
+;;; Packages & Modes
+
+;; repos
 (require 'package)
 (add-to-list 'package-archives'
 	     ("melpa-stable". "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
-;; clojure support
+;; clojure mode
 (require 'clojure-mode)
 
-;; mouse-support
-(require 'mouse)
-(xterm-mouse-mode t)
 
-;; ;; use xclip to copy/paste in emacs-nox
-;; (unless window-system
-;;   (when (getenv "DISPLAY")
-;;     (defun xclip-cut-function (text &optional push)
-;;       (with-temp-buffer
-;; 	(insert text)
-;; 	(call-process-region (point-min) (point-max)
-;; 			     "xclip" nil 0 nil "-i" "-selection" "clipboard")))
-;;     (defun xclip-paste-function ()
-;;       (let ((xclip-output (shell-command-to-string "xclip -o -selection clipboard")))
-;; 	(unless (string= (car kill-ring) xclip-output)
-;; 	  xclip-output)))
-;;     (setq interprogram-cut-function 'xclip-cut-function)
-;;     (setq interprogram-paste-function 'xclip-paste-function)
-;;     ))
-
-;; misc
+;;; Editing Style
 (setq require-final-newline t)
