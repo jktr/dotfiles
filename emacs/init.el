bb09741e98619547126b1455f068de90cea31bb1
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2015-11-14 04:10:23 +0100

    fix typo

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,6 @@
 ;;; UI
 
-## elements
+;; elements
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
