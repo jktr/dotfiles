06b90daadac624c343cae88bad2d2cb9bef7110f
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2016-10-03 01:03:52 +0200

    zsh: add statusof function

--- zsh/zshrc.d/80-functions.sh
+++ zsh/zshrc.d/80-functions.sh
@@ -1,29 +1,49 @@
-### functions.zshrc
+### functions.sh
 
 # prints process environment by pid or process name
 envof () {
-  case $1 in
-    ''|'-h'|'--help') # no args or help request
-      echo "Usage: envof (pid|progname)"
-      ;;
-    *[!0-9]*) # string has at least one non-numeric char
-      for pid in $(pidof $1); do
-        echo "----------- $pid -----------"
-        xargs --null --max-args=1 < "/proc/$pid/environ"
-      done
-      ;;
-    *) # args has only numeric chars
-      xargs --null --max-args=1 < "/proc/$1/environ"
-      ;;
-  esac
+    case $1 in
+        ''|'-h'|'--help') # no args or help request
+            echo "Usage: envof (pid|progname)"
+            ;;
+        *[!0-9]*) # string has at least one non-numeric char
+            for pid in $(pidof $1); do
+                echo "----------- $pid -----------"
+                xargs --null --max-args=1 < "/proc/${pid}/environ"
+            done
+            ;;
+        *) # args has only numeric chars
+            xargs --null --max-args=1 < "/proc/$1/environ"
+            ;;
+    esac
 }
+
+
+# prints process status by pid or process name
+statusof () {
+    case $1 in
+        ''|'-h'|'--help') # no args or help request
+            echo "Usage: statusof (pid|progname)"
+            ;;
+        *[!0-9]*) # string has at least one non-numeric char
+            for pid in $(pidof $1); do
+                echo "----------- $pid -----------"
+                cat "/proc/${pid}/status"
+            done
+            ;;
+        *) # args has only numeric chars
+            cat "/proc/$1/status"
+            ;;
+    esac
+}
+
 
 # prints largest directories by size on disk
 heavy () {
-  if [ -d "$1" ]; then
-    du --max-depth=1 -h "$1" | sort -hr
-  else
-    du --max-depth=1 -h "${PWD}" | sort -hr
-  fi
+    if [ -d "$1" ]; then
+        du --max-depth=1 -h "$1" | sort -hr
+    else
+        du --max-depth=1 -h "${PWD}" | sort -hr
+    fi
 }
 
