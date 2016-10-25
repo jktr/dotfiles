f83137d8dc7ec5d3f30ba45ccaded9178022cbad
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2016-10-25 19:50:03 +0200

    emacs: overhaul init; now drop-in base (instead of explicit activation)

--- emacs/init.el
+++ emacs/init.el
@@ -3,81 +3,38 @@
 
 ;; helpers
 
-(defun load-config (conf-dir conf-name)
-  (load (concat conf-dir "/" (symbol-name conf-name)))) 
+(defun ensure-repos (&rest repos)
+  (require 'package)
+  (mapc (apply-partially 'add-to-list 'package-archives) repos)
+  (setq package-user-dir "~/.emacs.d/elpa/")
+  (package-initialize)
+  (unless package-archive-contents
+    (package-refresh-contents)))
 
-(defun load-configs (conf-dir &rest configs)
-  (mapc (apply-partially 'load-config conf-dir) configs))
-       
-(defun load-package (conf-dir pkg)
-  (unless (package-installed-p pkg)
-    (package-install pkg))
-  (load-config conf-dir pkg))
+(defun ensure-config-dirs (&rest dirs)
+  (mapc (lambda (dir)
+          (mapc 'load (directory-files dir 't "\\.el$"))) dirs))
 
-(defun load-packages (conf-dir &rest pkgs)
-  (mapc (apply-partially 'load-package conf-dir) pkgs))
+(defun ensure-package-dirs (&rest dirs)
+  (mapc (lambda (dir)
+          (mapc (lambda (file)
+                  (let ((pkg (intern (file-name-base file))))
+                    (unless (package-installed-p pkg)
+                      (package-install pkg)
+                      (load-file file))))
+                (directory-files dir 't "\\.el$")))
+        dirs))
 
+;; init
 
-;; setup repos
+(ensure-repos
+ '("melpa-stable" . "http://stable.melpa.org/packages/")
+ '("marmalade"    . "http://marmalade-repo.org/packages/")
+)
 
-(require 'package)
-(mapc (apply-partially 'add-to-list 'package-archives)
-      '(("melpa-stable" . "http://stable.melpa.org/packages/")
-        ("marmalade"    . "http://marmalade-repo.org/packages/")))
-(setq package-user-dir "~/.emacs.d/elpa/")
-(package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
+(ensure-package-dirs
+ "~/.emacs.d/config-pkg")
 
-
-;; set up basic configuration for emacs
-
-(load-configs "~/.emacs.d/config"
-              'wm ; navigation & components
-              'color
-              'font
-              'syntax ; whitespace & encoding
-              'input-devices
-;              'backup
-              )
-
-
-;; set up repo-based packages, downloading as needed
-(load-packages "~/.emacs.d/config-pkg"
-  ;; syntax
-               'smart-tabs-mode
-               'flycheck
-  ;; autocomplete
-               'auto-complete
-               'which-key
-  ;; appearance
-               'darcula-theme
-               'rainbow-delimiters
-               'rainbow-mode
-  ;; markdown
-               'markdown-mode
-  ;; lisp
-               'paredit
-  ;; clojure
-               'clojure-mode
-               'clojure-mode-extra-font-locking
-               'cider ; clj* repl
-               )
-
-
-;; set up externally installed packages
-
-(load-configs "~/.emacs.d/config-extern"           
-             'auctex  ; from distro package manager
-             'rainbow ; shipped with emacs
-             )
-
-
-;; clean up helpers
-
-(mapc 'fmakunbound
-      '(load-config
-        load-configs
-        load-package
-        load-packages))
-
+(ensure-config-dirs
+ "~/.emacs.d/config"
+ "~/.emacs.d/config-extern")
