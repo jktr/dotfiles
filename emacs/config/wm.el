50216fef5497a468339eba952029ebb34332e88a
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2016-10-29 23:13:25 +0200

    emacs: change window resize keybind

--- emacs/config/wm.el
+++ emacs/config/wm.el
@@ -1,26 +1,24 @@
 ;;; wm.el
-;;; configure ui components and corresponign keybinds
+;;; configure ui components and frame/window-related keybinds
+
+;; ui elements
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq column-number-mode  1)
+(setq inhibit-startup-screen t)
+
+;; resize windows
+(global-set-key (kbd "M-S-<left>")  'shrink-window-horizontally)
+(global-set-key (kbd "M-S-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "M-S-<down>")  'shrink-window)
+(global-set-key (kbd "M-S-<up>")    'enlarge-window)
+
+;; move windows
+(global-set-key (kbd "M-<left>")  'windmove-left)
+(global-set-key (kbd "M-<right>") 'windmove-right)
+(global-set-key (kbd "M-<up>")    'windmove-up)
+(global-set-key (kbd "M-<down>")  'windmove-down)
 
 ;; disable suspend keybind
 (global-unset-key (kbd "C-z"))
-
-;; elements
-(when (fboundp 'menu-bar-mode)   (menu-bar-mode -1))
-(when (fboundp 'tool-bar-mode)   (tool-bar-mode -1))
-(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(setq inhibit-startup-screen t)
-
-;; column display
-(setq column-number-mode 1)
-
-;; resize windows
-(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
-(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "S-C-<down>") 'shrink-window)
-(global-set-key (kbd "S-C-<up>") 'enlarge-window)
-
-;; move windows
-(global-set-key (kbd "M-<left>") 'windmove-left)
-(global-set-key (kbd "M-<right>") 'windmove-right)
-(global-set-key (kbd "M-<up>") 'windmove-up)
-(global-set-key (kbd "M-<down>") 'windmove-down)
