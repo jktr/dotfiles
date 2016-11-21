93711408991b34d4aa6e2f897fd396faebb00bf4
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2016-11-21 22:20:00 +0100

    remove unused keybinds

--- zsh/zshrc.d/80-keybinds.sh
+++ zsh/zshrc.d/80-keybinds.sh
@@ -6,30 +6,3 @@ user-kill-word-first () {
 }
 zle -N user-kill-word-first
 bindkey '^[w' user-kill-word-first
-
-user-prepend () {
-    read -k str
-    case "${str}" in
-    s) str='sudo ' ;;
-    m) str='man '  ;;
-    e) str='$EDITOR ' ;;
-    *) return 0 ;;
-    esac
-    zle beginning-of-line
-    zle -U ${str}
-}
-zle -N user-prepend
-bindkey '^[e' user-prepend
-
-user-append () {
-    read -k str
-    case "${str}" in
-    l) str=' | less' ;;
-    *) return 0 ;;
-    esac
-    zle end-of-line
-    zle -U ${str}
-}
-zle -N user-append
-bindkey '^[r' user-append
-
