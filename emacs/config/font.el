41e6a43d8b0d79b7bf9e72315c85ea541bf05cfb
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2016-11-27 20:43:35 +0100

    cleanup: remove obsolete commented code

--- emacs/config/font.el
+++ emacs/config/font.el
@@ -1,14 +1,6 @@
 ;;; font.el
 ;;; customize general font defaults
 
-;; set font size after frame is made
-;; (if (daemonp)
-;;     (add-hook 'after-make-frame-functions
-;;               (lambda (frame)
-;;                 (with-selected-frame frame
-;;                   (set-face-attribute 'default nil :height 95))))
-;;   (set-face-attribute 'default nil :height 95))
-
 (let ((setup-font-for-frame (lambda (frame)
                               (set-face-attribute
                                'default frame
