99a27fe71d99e044efe54b94d025784b9a1ca6e5
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2017-02-08 00:32:32 +0100

    emacs: fix window move/resize keybinds to work in a linux console

--- emacs/config/wm.el
+++ emacs/config/wm.el
@@ -8,20 +8,20 @@
 (setq column-number-mode  1)
 (setq inhibit-startup-screen t)
 
-;; resize windows
-(global-set-key (kbd "M-S-<left>")  'shrink-window-horizontally)
-(global-set-key (kbd "M-S-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "M-S-<down>")  'shrink-window)
-(global-set-key (kbd "M-S-<up>")    'enlarge-window)
-
 ;; let the WM (if graphical) manage windows
 (setq pop-up-frames 'graphic-only)
 
-;; move windows
-(global-set-key (kbd "M-<left>")  'windmove-left)
-(global-set-key (kbd "M-<right>") 'windmove-right)
-(global-set-key (kbd "M-<up>")    'windmove-up)
-(global-set-key (kbd "M-<down>")  'windmove-down)
+;; move between windows
+(global-set-key (kbd "C-c h") 'windmove-left)
+(global-set-key (kbd "C-c j") 'windmove-down)
+(global-set-key (kbd "C-c k") 'windmove-up)
+(global-set-key (kbd "C-c l") 'windmove-right)
+
+;; resize windows
+(global-set-key (kbd "C-c C-h") 'shrink-window-horizontally)
+(global-set-key (kbd "C-c C-j") 'shrink-window)
+(global-set-key (kbd "C-c C-k") 'enlarge-window)
+(global-set-key (kbd "C-c C-l") 'enlarge-window-horizontally)
 
 ;; disable suspend keybind
 ;;   * next to massively overloaded C-x
