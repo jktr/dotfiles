d3fa7a08327e042c0ab4b5363e657a51688810b6
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2017-02-08 00:27:25 +0100

    emacs: enable one-on-one mode when graphical

--- emacs/config/wm.el
+++ emacs/config/wm.el
@@ -14,6 +14,9 @@
 (global-set-key (kbd "M-S-<down>")  'shrink-window)
 (global-set-key (kbd "M-S-<up>")    'enlarge-window)
 
+;; let the WM (if graphical) manage windows
+(setq pop-up-frames 'graphic-only)
+
 ;; move windows
 (global-set-key (kbd "M-<left>")  'windmove-left)
 (global-set-key (kbd "M-<right>") 'windmove-right)
@@ -21,4 +24,7 @@
 (global-set-key (kbd "M-<down>")  'windmove-down)
 
 ;; disable suspend keybind
+;;   * next to massively overloaded C-x
+;;   * wrecks havok when running a tiling window manager
+;;   * server's running anyways, so C-x-5 doesn't loose any work
 (global-unset-key (kbd "C-z"))
