138fe4bf73dfba65af6e9396a0a740720c7490df
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2017-07-20 02:09:24 +0200

    zsh: add util for terminal colors

--- zsh/zshrc.d/85-fn-misc.sh
+++ zsh/zshrc.d/85-fn-misc.sh
@@ -24,3 +24,15 @@ ttyinit () {
 wttr () {
     curl "https://wttr.in/$@" 2>/dev/null | $PAGER -r
 }
+
+# print colors w/ escape codes
+colortest () {
+    for x in {0..8}; do
+        for i in {30..37}; do
+            for a in {40..47}; do
+                echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "
+            done
+            echo
+        done
+    done
+}
