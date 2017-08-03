2e3bd119c666a73dbdb2bca141d5e06b88567df6
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2017-08-03 22:48:00 +0200

    zsh: pm: add 'files' and 'who' command

--- zsh/zshrc.d/85-fn-package-manager.zsh
+++ zsh/zshrc.d/85-fn-package-manager.zsh
@@ -12,13 +12,15 @@ declare -A _pm
 ## populate look-up table
 if >/dev/null command -v 'zypper'; then
     _pm=(
-        'if'   'zypper info'
-        'in'   'sudo zypper install'
-        'list' 'zypper search'
-        'ref'  'sudo zypper refresh'
-        'rm'   'sudo zypper remove'
-        'se'   'zypper search'
-        'up'   'sudo zypper upgrade'
+        'files' 'rpm --query --list'
+        'if'    'zypper info'
+        'in'    'sudo zypper install'
+        'list'  'zypper search'
+        'ref'   'sudo zypper refresh'
+        'rm'    'sudo zypper remove'
+        'se'    'zypper search'
+        'up'    'sudo zypper upgrade'
+        'who'   'zypper search --provides --match-exact'
     )
 elif >/dev/null command -v 'pacman'; then
     _pm=(
@@ -32,33 +34,39 @@ elif >/dev/null command -v 'pacman'; then
     )
 elif >/dev/null command -v 'apt'; then
     _pm=(
-        'if'   'apt show'
-        'in'   'sudo apt install'
-        'list' 'dpkg --list'
-        'ref'  'sudo apt update'
-        'rm'   'apt purge'
-        'se'   'apt search'
-        'up'   'sudo apt upgrade'
+        'files' 'pacman --query --list --quiet'
+        'if'    'pacman --sync --info'
+        'in'    'sudo pacman --sync'
+        'list'  'pacman --query'
+        'ref'   'sudo pacman --sync --refresh'
+        'rm'    'sudo pacman --remove --nosave --recursive --cascade'
+        'se'    'pacman --sync --search'
+        'up'    'sudo pacman --sync --sysupgrade'
+        'who'   'pacman --query --owns'
     )
 elif >/dev/null command -v 'apt-get'; then
     _pm=(
-        'if'   'apt-cache show'
-        'in'   'sudo apt-get install'
-        'list' 'dpkg --list'
-        'ref'  'sudo apt-get update'
-        'rm'   'sudo apt-get purge'
-        'se'   'apt-cache search'
-        'up'   'sudo apt-get dist-upgrade'
+        'files' 'dpkg-query --listfiles'
+        'if'    'apt-cache show'
+        'in'    'sudo apt-get install'
+        'list'  'dpkg --list'
+        'ref'   'sudo apt-get update'
+        'rm'    'sudo apt-get purge'
+        'se'    'apt-cache search'
+        'up'    'sudo apt-get dist-upgrade'
+        'who'   'dpkg --search'
     )
 elif >/dev/null command -v 'apk'; then
     _pm=(
-        'if'   'apk info'
-        'in'   'su - apk add --no-cache'
-        'list' 'dpkg --list'
-        'ref'  'su - apk update'
-        'rm'   'su - apk del'
-        'se'   'apk search'
-        'up'   'su - apk upgrade'
+        'files' 'apk info --contents'
+        'if'    'apk info'
+        'in'    'su - apk add --no-cache'
+        'list'  'dpkg --list'
+        'ref'   'su - apk update'
+        'rm'    'su - apk del'
+        'se'    'apk search'
+        'up'    'su - apk upgrade'
+        'who'   'apk info --who-owns'
     )
 fi
 
