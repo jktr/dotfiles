c9e0cc01cda1caf3a5326be1dc05e1b8139b2a53
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2018-02-01 22:29:33 +0100

    zsh: clean up history settings

--- zsh/zshrc.d/50-history.sh
+++ zsh/zshrc.d/50-history.sh
@@ -1,15 +1,22 @@
 ### history
 
-setopt APPEND_HISTORY
-setopt HIST_FCNTL_LOCK
+# cleaner history
 setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_SPACE
 setopt HIST_NO_FUNCTIONS
 setopt HIST_NO_STORE
 setopt HIST_REDUCE_BLANKS
+
+# prevent user error
 setopt HIST_VERIFY
+setopt HIST_IGNORE_SPACE
+
+# workaround for nfs issues
+setopt HIST_FCNTL_LOCK
+
+# multi-vt workflow
+setopt APPEND_HISTORY
 setopt INC_APPEND_HISTORY
 
 HISTFILE=~/.zsh_history
-HISTSIZE=1000
-SAVEHIST=10000
+HISTSIZE=2000
+SAVEHIST=25000
