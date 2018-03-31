3a55926448435d9af16b340901c51687bfb87d60
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2018-04-01 00:50:01 +0200

    zsh: drop ssh-keygen fn
    This only lead to painful confusions.

--- zsh/zshrc.d/85-fn-misc.sh
+++ zsh/zshrc.d/85-fn-misc.sh
@@ -1,27 +1,5 @@
 ### fn-misc
 
-# generates new ssh key
-# $1 destination
-# $2 identity
-# $3 link as
-ssh-keygen () {
-    [ -n "$1" ] || { echo must provide destination && return 1; }
-    [ -n "$2" ] || { echo must provide identity && return 1; }
-    local -r key=~/.ssh/keys/"id_$(date +%Y-%m-%d)_ed25519_$1-$2"
-    local -r pub="${key}.pub"
-    command ssh-keygen -t ed25519 -f "$key" -C "$HOST::$1::$2"
-    [ -n "$3" ] && {
-        ln -sf ${key} ~/.ssh/keys/"${3}.key"
-        local -r conf=~/.ssh/config.d/"${3}.conf"
-        [ -e "$conf" ] || cat > "${conf}" <<EOF
-Host ${3}
-
-    User $2
-    Port 22
-EOF
-    }
-}
-
 # looks up args via duck duck go
 w3d () {
     local -r params="$*"
