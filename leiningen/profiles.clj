e667210c49d83585c3e69b126d42b1d1028cbe0e
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2018-06-15 19:10:07 +0200

    lein: bump plugins

--- leiningen/profiles.clj
+++ leiningen/profiles.clj
@@ -2,5 +2,5 @@
 ;;; lein user profile
 
 {:user
- {:plugins [[lein-ancient "0.5.5"]
-            [lein-midje   "3.1.3"]]}}
+ {:plugins [[lein-ancient "0.6.15"]
+            [lein-midje   "3.2.1"]]}}
