532177fd1cb0fe48376755c4355020b43082e275
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2019-02-20 06:56:15 +0100

    zsh: remove obsolete utils (uses udev/systemd now)

--- zsh/zshrc.d/85-fn-laptop.sh
+++ zsh/zshrc.d/85-fn-laptop.sh
@@ -1,16 +1,5 @@
 ### fn-laptop
 
-# retrieves current battery level
-battery_level () {    
-    local -r bat=/sys/class/power_supply/BAT1
-
-    full=$(< "${bat}/charge_full")
-    now=$(< "${bat}/charge_now")
-
-    percent=$(echo "100*($now/$full)" | bc -l )
-    echo "$bat: ${full}/${now} -> ${percent}"
-}
-
 # sets backlight brightness
 brightness () {
     local -r max=950
@@ -24,13 +13,11 @@ brightness () {
         *[!0-9]*) # > 0 non-numeric
             echo 'NaN...'
             echo "$useage"
-            exit 1
             ;;
         *) # only numeric
             if [ $1 -gt $max -o $1 -lt $min ]; then
                 echo 'Outside range'
                 echo "$usage"
-                exit 1
             else
                 sudo sh -c \
                      "echo $1 > /sys/class/backlight/intel_backlight/brightness"
@@ -38,6 +25,3 @@ brightness () {
             ;;
     esac
 }
-
-# man sets /dev/sda to contiuouly spin to reduce wear
-alias nosleep-sda='sudo hdparm -B 128 -S 60 /dev/sda'
