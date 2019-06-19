0958c730dd97c635a68f96d1658ae885a4b10cbd
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2019-06-19 14:17:11 +0200

    firefox: prefer dark theme

--- firefox/user.js
+++ firefox/user.js
@@ -19,3 +19,6 @@ user_pref("font.name.serif.x-western", "sans-serif");
 user_pref("general.warnOnAboutConfig", false); // I'll be careful; I promise!
 user_pref("middlemouse.contentLoadURL", false); // X11 PRIMARY Selection
 user_pref("reader.parse-on-load.enabled", false);
+
+// theming
+user_pref("ui.systemUsesDarkTheme", 1);
