a277bb0d2b121010115e46f08a4876a44f59ec25
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2019-09-15 15:07:13 +0200

    zsh: rework completion system configs

--- zsh/zshrc.d/50-completion.sh
+++ zsh/zshrc.d/50-completion.sh
@@ -1,19 +1,17 @@
 ### completion
 
-zstyle ':completion:*' completer _complete _ignored
+zstyle ':completion:*' completer _extensions _complete _list _approximate
+
 zstyle ':completion:*' expand prefix suffix
+zstyle ':completion:*' file-sort name
 zstyle ':completion:*' group-name '%d'
-zstyle ':completion:*' ignore-parents pwd
+zstyle ':completion:*' ignore-parents pwd ..
 zstyle ':completion:*' insert-unambiguous true
-zstyle ':completion:*' list-colors ''
-zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' list-suffixes true
-zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]}' 'r:|[._-]=* r:|=*' ''
+zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' 'r:|[._-]=* r:|=*'
+zstyle ':completion:*' max-errors 2
 zstyle ':completion:*' menu select=long
-zstyle ':completion:*' original false
-zstyle ':completion:*' verbose true
-zstyle ':completion:*' preserve-prefix '//[^/]##/'
-zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
 zstyle ':completion:*' use-compctl false
-zstyle :compinstall filename '/home/jktr/.zshrc'
-autoload -Uz compinit && compinit
+
+autoload -Uz compinit && compinit -d "$XDG_CACHE_HOME/zcompdump"
