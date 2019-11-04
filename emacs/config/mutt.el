a68b64d95b36cd2abec8998285906effdd071529
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2019-11-05 00:03:09 +0100

    emacs: also use message-mode for neomutt

--- emacs/config/mutt.el
+++ emacs/config/mutt.el
@@ -1,4 +1,5 @@
 ;;; mutt.el
 ;;; nicities when using mutt to compose email
 
-(add-to-list 'auto-mode-alist '("^/tmp/mutt.*" . message-mode))
+(add-to-list 'auto-mode-alist '("^/tmp/mutt-.*" . message-mode))
+(add-to-list 'auto-mode-alist '("^/tmp/neomutt-.*" . message-mode))
