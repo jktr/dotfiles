da8f64051affe5a3069409a3eb4379ae749b1f29
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2020-01-25 18:22:21 +0100

    zsh: add zle compat for more keys

--- zsh/zshrc.d/10-zle-compat.sh
+++ zsh/zshrc.d/10-zle-compat.sh
@@ -4,6 +4,8 @@ bindkey -e
 typeset -g -A key
 
 # compat aliases
+# ref: infocmp(1), terminfo(5)
+# ref: https://invisible-island.net/ncurses/terminfo.src.html#tic-xterm-256color
 key[Home]=${terminfo[khome]}
 key[End]=${terminfo[kend]}
 key[Insert]=${terminfo[kich1]}
@@ -11,9 +13,12 @@ key[Delete]=${terminfo[kdch1]}
 key[Up]=${terminfo[kcuu1]}
 key[Down]=${terminfo[kcud1]}
 key[Left]=${terminfo[kcub1]}
+key[AltLeft]=${terminfo[kLFT3]}
 key[Right]=${terminfo[kcuf1]}
+key[AltRight]=${terminfo[kRIT3]}
 key[PageUp]=${terminfo[kpp]}
 key[PageDown]=${terminfo[knp]}
+key[F1]=${terminfo[kf1]}
 
 # bind keys to zsh's readline replacement
 [[ -n "${key[Home]}"     ]] && bindkey "${key[Home]}"     beginning-of-line
@@ -23,7 +28,9 @@ key[PageDown]=${terminfo[knp]}
 [[ -n "${key[Up]}"       ]] && bindkey "${key[Up]}"       up-line-or-history
 [[ -n "${key[Down]}"     ]] && bindkey "${key[Down]}"     down-line-or-history
 [[ -n "${key[Left]}"     ]] && bindkey "${key[Left]}"     backward-char
+[[ -n "${key[AltLeft]}"  ]] && bindkey "${key[AltLeft]}"  emacs-backward-word
 [[ -n "${key[Right]}"    ]] && bindkey "${key[Right]}"    forward-char
+[[ -n "${key[AltRight]}" ]] && bindkey "${key[AltRight]}" emacs-forward-word
 
 # compatibility for console mode applications
 if [[ -n ${terminfo[smkx]} ]] && [[ -n ${terminfo[rmkx]} ]]; then
