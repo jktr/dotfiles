dcd9cbf511f9717329ce6f570e34c2a4a1b03218
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2020-03-07 01:42:10 +0100

    zsh: merge shopt/umask/history configs

--- zsh/zshrc.d/10-shopt.sh
+++ zsh/zshrc.d/10-shopt.sh
@@ -1,4 +1,4 @@
-### shopt
+### shell settings
 
 # globbing
 unsetopt NULL_GLOB
@@ -14,5 +14,35 @@ setopt INTERACTIVE_COMMENTS
 setopt RM_STAR_WAIT
 unsetopt BEEP
 
-# modules
+## umask
+if [ $UID -eq 0 ]; then
+    umask -S u=rwx,g=rx,o=rx
+else
+    umask -S u=rwx,g=,o=
+fi
+
+## history
+
+# cleaner history
+setopt HIST_IGNORE_DUPS
+setopt HIST_NO_FUNCTIONS
+setopt HIST_NO_STORE
+setopt HIST_REDUCE_BLANKS
+
+# prevent user error
+setopt HIST_VERIFY
+setopt HIST_IGNORE_SPACE
+
+# workaround for nfs issues
+setopt HIST_FCNTL_LOCK
+
+# multi-vt workflow
+setopt INC_APPEND_HISTORY
+unsetopt SHARE_HISTORY
+
+HISTFILE=~/.zsh_history
+HISTSIZE=2000
+SAVEHIST=25000
+
+## modules
 autoload -U colors && colors
--- zsh/zshrc.d/10-umask.sh
+++
@@ -1,7 +0,0 @@
-### umask
-
-if [ $UID -eq 0 ]; then
-    umask -S u=rwx,g=rx,o=rx
-else
-    umask -S u=rwx,g=,o=
-fi
